5ba82e27db957e3a216f52ee9bed765203e5fdde
Andy Liu <liuandy284@gmail.com>
2026-08-27 20:18:11 -0500

    Implement drawMap function in main.cpp
    Added a drawMap function to display a bordered grid.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 
+
+constexpr int kW = 28;
+constexpr int kL = 60;
+
+void drawMap(){
+    system("cls");
+    std::cout << "+";
+    for(int i = 0; i < kL; i ++){
+        std::cout << "-";
+    }
+    std::cout << "+" << std::endl;
+    for(int i = 0; i < kW; i ++){
+        std::cout << "|";
+        for(int j = 0; j < kL; j ++){
+            std::cout << " ";
+        }
+        std::cout << "|" << std::endl;
+    }
+    std::cout << "+";
+    for(int i = 0; i < kL; i ++){
+        std::cout << "-";
+    }
+    std::cout << "+" << std::endl;
+}
+
+
 int main(){
-    int count = 0;
+
+    drawMap();
+
     while(1){
-        count ++;
-        std::cout << count << std::endl;
+
     }
 
 
