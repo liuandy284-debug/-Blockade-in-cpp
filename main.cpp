76ae12d4042f040f682588c1bdd91cf800be669a
Andy Liu <liuandy284@gmail.com>
2026-09-02 19:59:51 -0500

    Refactor map drawing and initialization logic
    Added a Map structure and initialization function for game state.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,28 @@
 constexpr int kW = 28;
 constexpr int kL = 60;
 
-void drawMap(){
+enum BlockType{
+
+     EMPTY = 0,
+     FOOD = 1,
+};
+
+struct Map {
+    BlockType map[kW][kL];
+    bool isFoodEaten;
+};
+
+void initMap(Map* map){
+    for(int i = 0; i < kW; i ++){
+        for(int j = 0; j < kL; j ++){
+            map->map[i][j] = BlockType::EMPTY;
+        }
+    }
+    map->isFoodEaten = false;
+}
+
+
+void drawMap(Map* map){
     system("cls");
     std::cout << "+";
     for(int i = 0; i < kL; i ++){
@@ -14,7 +35,12 @@ void drawMap(){
     for(int i = 0; i < kW; i ++){
         std::cout << "|";
         for(int j = 0; j < kL; j ++){
-            std::cout << " ";
+            if(map->map[i][j] == BlockType::EMPTY){
+                std::cout << " ";
+            }
+            else if(map->map[i][j] == BlockType::FOOD){
+                std::cout << "F";
+            }
         }
         std::cout << "|" << std::endl;
     }
@@ -28,8 +54,12 @@ void drawMap(){
 
 int main(){
 
-    drawMap();
+    Map map;
+    initMap(&map);
+    drawMap(&map);
 
+
+    //Creating a main loop 
     while(1){
 
     }
